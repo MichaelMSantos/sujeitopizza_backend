8afc39ee5a0eebceddf986fca87c7ae050dcdb9c
Michael M Santos <mmss240610@gmail.com>
2025-03-16 16:57:34 -0300

    hotfix server tmp

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,19 @@
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors';
 import cors from 'cors';
-import path from 'path'
 
 import { router } from './routes'
 import fileUpload from 'express-fileupload';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(fileUpload({
   limits: {
     fileSize: 50 * 1024 * 1024
   }
 }))
-app.use(cors());
-
 app.use(router);
-
-app.use(
-  '/files',
-  express.static(path.resolve(__dirname, '..', 'tmp'))
-)
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
